6d84af690b71365af74ef7cd6a65c9221fb07403
javierroberts <javier.roberts@hotmail.com>
2020-02-28 22:41:14 -0500

    Fixed issue with json retrieval

--- test_feedTools.py
+++ test_feedTools.py
@@ -5,12 +5,13 @@ import json
 import testImage
 
 with open('nytimes.json') as json_file:
-    status_json = json.load(json_file)
+    json_data = json.load(json_file)
 
 
-def test_getFeed(capsys):
+def test_getFeed():
     if os.stat("keys").st_size == 0:
-        assert status_json['text'] == "The public health director of Santa Clara County, California, confirmed that the county's new coronavirus case does"
+        status_json = json.loads(json_data)
+        assert status_json['text'] == "The public health director of Santa Clara County, California, confirmed that the county's new coronavirus case does… https://t.co/hl8ClztVbL"
     else:
         data = ft.getFeed("BleacherReport", 1)
         assert len(data[0]['text']) > 0
@@ -22,3 +23,7 @@ def test_annotateImage(capsys):
     else:
         description = ft.annotateImage(testImage.url)
         assert description == "Blue, Cool, T-shirt, Outerwear, Facial hair, Beard, Rapper, Sleeve, Top"
+
+
+if __name__ == "__main__":
+    test_getFeed()
